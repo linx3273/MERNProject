554570c1b23d0553d5726800ec0c0534c3a470b7
linx3273 <ayush96.gupta@gmail.com>
2021-12-08 19:08:55 +0530

    added few basic pages along with routing for frontend component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import Home from './components/Home'
+import Catalogue from './components/Catalogue';
+import About from './components/About';
+import Login from './components/Login';
+import Register from './components/Register';
+import {Route,Routes} from "react-router-dom";
 
 const App = () =>{
   return (
-    <div>
-      <h1>Home?</h1>
-    </div>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/catalogue' element={<Catalogue/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/Register' element={<Register/>} />
+      </Routes>
+    </>
   )
 }
 
